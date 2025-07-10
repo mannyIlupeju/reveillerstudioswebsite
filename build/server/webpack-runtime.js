13a94ef47493f6ae5d70109425afa28bd2ce60b8
machnmb_ <mannybiz@outlook.com>
2025-07-10 18:59:05 -0400

    Untracked .gltf from Git LFS and added it as a regular file

--- build/server/webpack-runtime.js
+++ build/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("4c6379c6d50341c6")
+/******/ 		__webpack_require__.h = () => ("7cc3c521c38ec81d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
